a245b534934190c2c864e40e62d2b6aeb231ac7b
Mike <mikeboldsen2004@gmail.com>
2023-03-24 22:27:57 +0100

    basics down for landingpage

---
+++ Scripts/index.js
@@ -0,0 +1,22 @@
+import { releases } from "../Scripts/hero.js";
+console.log(releases)
+
+const releaseText = document.querySelector('.text');
+const artworkContainer = document.querySelector('.artwork-container')
+
+const artworkContainerTmp = (releases) => 
+`<img src="${releases[0].image}" class="release-artwork" alt="">`
+
+const releaseTmp = (releases) => 
+`<div class="text"><h1 class="heading">Sanctuary</h1>
+<p class="release">${releases[0].title} by ${releases[0].artist} is out now!</p>
+<a href="${releases[0].link}" target="_blank"><button class="streamnow">Stream Now <i class="fa-solid fa-up-right-from-square fa-bounce"></i></button></a>`
+
+if (releaseText) {
+    releaseText.innerHTML = releaseTmp(releases);
+}
+
+if(artworkContainer) {
+    artworkContainer.innerHTML = artworkContainerTmp(releases);
+}
+
